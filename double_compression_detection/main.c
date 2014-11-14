b41cdc5c0be1132bd9da5af3c864a5722cec5ef9
Daniel Kocher <daniel.kocher@stud.sbg.ac.at>
2014-11-14 15:13:31 +0100

    Added exec call to run decompression.

--- double_compression_detection/main.c
+++ double_compression_detection/main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-
-#include "T.835/jpegxr.h"
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <assert.h>
+#include <unistd.h>
 
 int main(int argc, char** argv) {
-	jxr_image_t image = NULL;
+	int ret;
+	char *cmd[] = {"jpegxr", "../pictures/lena512.jxr", (char *)0};
+	ret = execv("T.835/jpegxr", cmd);
 
-	return 0;
+	return ret;
 }
