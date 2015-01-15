a4ea6a02e49565b4d912b126ab080a6292dab0a2
danielkocher <Daniel.Kocher@stud.sbg.ac.at>
2015-01-15 02:04:05 +0100

    Shell script to execute everything using a single command.

--- double_compression_detection/exec.sh
+++ double_compression_detection/exec.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-sh cleanup.sh
+# echo on
+set -x
 
-#make
+sh clean.sh
 make
-
-#execute
-./main ../pictures/mandrill_4.2.03.jxr mandrill_4.2.03.tif
-
-#histograms
-for file in ./hist*.dat ; do
-	fn_in=$(basename "$file")
-
-	fn_out=$(basename "$file")
-	fn_out="${fn_out%.*}.png"
-	
-	gnuplot -e "fn_in='$fn_in'; fn_out='$fn_out'" hist.gnu
-done
+./main
+cd test_images/extracted/standard_test_images/double_compressed
+sh generate_histograms.sh
+#cd ../../../..
