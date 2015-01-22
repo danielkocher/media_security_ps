087d7eba8fc71382cc7b0aacc60bc221be28fe6a
Daniel Kocher <daniel.kocher@stud.sbg.ac.at>
2015-01-22 18:49:26 +0100

    Added single compressed file generation.

--- test_images/generate.sh
+++ test_images/generate.sh
@@ -9,25 +9,31 @@ for f in $FOLDER_ORIGINAL
 do
 	for i in $@
 	do
+		fn=$(basename "$f")
+		fn="${fn%.*}"
+		fntmpjxr="$fn-$i.jxr"
+		fntmptif="$fn-$i.tif"
+
+		mkdir "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i" && \
+		${PATH_JPEGXR} -c -o "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i/$fntmpjxr" -q $i "$f" && \
+		${PATH_JPEGXR} -o "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i/$fntmptif" "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i/$fntmpjxr"
+
+		echo "Single-compressed file $f using quantisation $i. Result file: $fntmpjxr" && \
+		rm -f "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i/$fntmptif" "${FOLDER_DOUBLE_COMPRESSED}$fn-$i/coeffs.csv"
+
 		for j in $@
 		do
 			if [ $i -ne $j ]
 			then
-				fn=$(basename "$f")
-				fn="${fn%.*}"
-
-				fntmpjxr="$fn-$i.jxr"
-				fntmptif="$fn-$i.tif"
 				fnfinaljxr="$fn-$i-$j.jxr"
 				
 				mkdir "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i-$j" && \
-				${PATH_JPEGXR} -c -o "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i-$j/$fntmpjxr" -q $i "$f" && \
-				${PATH_JPEGXR} -o "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i-$j/$fntmptif" "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i-$j/$fntmpjxr" && \
+				${PATH_JPEGXR} -o "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i-$j/$fntmptif" "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i/$fntmpjxr" && \
 				${PATH_JPEGXR} -c -o "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i-$j/$fnfinaljxr" -q $j "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i-$j/$fntmptif"
 
 				#touch $fntmpjxr $fntmptif $fnfinaljxr
-				echo "Double-compressed file $f using quantisation combination ($i, $j). Result file: $fnfinaljxr"
-				rm -f "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i-$j/$fntmpjxr" "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i-$j/$fntmptif" coeffs.csv
+				echo "Double-compressed file $f using quantisation combination ($i, $j). Result file: $fnfinaljxr" && \
+				rm -f "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i-$j/$fntmptif" "${FOLDER_DOUBLE_COMPRESSED}$fn-$i-$j/coeffs.csv"
 			fi
 		done
 	done
