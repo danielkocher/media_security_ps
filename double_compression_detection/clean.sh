be2619b1c298cdc5fd1038869d93594a7193f16c
danielkocher <Daniel.Kocher@stud.sbg.ac.at>
2015-02-27 01:00:59 +0100

    Added gnu, sh and quantify to the cleanup and changed directory to be considered.

--- double_compression_detection/clean.sh
+++ double_compression_detection/clean.sh
@@ -3,4 +3,4 @@
 # echo on
 set -x
 
-find . -name "*.tif" -o -name "*.csv" -o -name "*.dat" -o -name "*.png" -type f | xargs rm -f
+find test_images -name "*.tif" -o -name "*.csv" -o -name "*.dat" -o -name "*.png" -o -name "*.gnu" -o -name "*.sh" -o -name "quantify" -type f | xargs rm -f
