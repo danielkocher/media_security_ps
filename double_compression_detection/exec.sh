b9a8ca9b808f13ef03b6979c43a1083a5f13ff4a
danielkocher <Daniel.Kocher@stud.sbg.ac.at>
2015-02-27 01:01:24 +0100

    Correct exec.sh (the ordering is relevant).

--- double_compression_detection/exec.sh
+++ double_compression_detection/exec.sh
@@ -6,19 +6,21 @@ set -x
 #Clean
 sh clean.sh
 
-#Build & Prepare
+#Build
 make
-cd histogram
-cp exec.sh ../test_images/extracted/standard_test_images/double_compressed/generate_histograms.sh
-cd ..
-cd quantify
-make
-cp quantify ../test_images/extracted/standard_test_images/double_compressed/quantify
-cd ..
 
-#Run
+#Run decoding
 ./main test_images
+
+#Copy files to generate histograms and quantifications
+cp ./histogram/exec.sh ./test_images/extracted/standard_test_images/double_compressed/generate_histograms.sh
+cp ./histogram/hist.gnu ./test_images/extracted/standard_test_images/double_compressed/hist.gnu
+cp ./quantify/quantify ./test_images/extracted/standard_test_images/double_compressed/quantify
+cp ./quantify/do_quantification.sh ./test_images/extracted/standard_test_images/double_compressed/do_quantification.sh
+
+#Run histogram generation and quantification
 cd test_images/extracted/standard_test_images/double_compressed
 #sh generate_histograms.sh
 sh do_quantification.sh
+
 #cd ../../../..
