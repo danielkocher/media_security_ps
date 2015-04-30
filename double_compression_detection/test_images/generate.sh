16e5697dbfb3974238a205ebf2fb43f07bcc3d80
Daniel Kocher <Daniel.Kocher@stud.sbg.ac.at>
2015-04-30 19:13:08 +0200

    Fixed bug. Script now deletes coeffs.csv files too

--- double_compression_detection/test_images/generate.sh
+++ double_compression_detection/test_images/generate.sh
@@ -7,8 +7,8 @@ fi
 
 FOLDER_BASE="extracted"
 FOLDER_ORIGINAL="${FOLDER_BASE}/original/*.tif"
-FOLDER_DOUBLE_COMPRESSED="${FOLDER_BASE}/compressed"
-PATH_JPEGXR="../double_compression_detection/T.835/jpegxr"
+FOLDER_COMPRESSED="${FOLDER_BASE}/compressed"
+PATH_JPEGXR="../T.835/jpegxr"
 
 for f in $FOLDER_ORIGINAL
 do
@@ -19,12 +19,12 @@ do
 		fntmpjxr="$fn-$i.jxr"
 		fntmptif="$fn-$i.tif"
 
-		mkdir "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i" && \
-		${PATH_JPEGXR} -c -o "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i/$fntmpjxr" -q $i "$f" && \
-		${PATH_JPEGXR} -o "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i/$fntmptif" "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i/$fntmpjxr"
+		mkdir "${FOLDER_COMPRESSED}/$fn-$i" && \
+		${PATH_JPEGXR} -c -o "${FOLDER_COMPRESSED}/$fn-$i/$fntmpjxr" -q $i "$f" && \
+		${PATH_JPEGXR} -o "${FOLDER_COMPRESSED}/$fn-$i/$fntmptif" "${FOLDER_COMPRESSED}/$fn-$i/$fntmpjxr"
 
 		echo "Single-compressed file $f using quantisation $i. Result file: $fntmpjxr" && \
-		rm -f "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i/$fntmptif" "${FOLDER_DOUBLE_COMPRESSED}$fn-$i/coeffs.csv"
+		rm -f "${FOLDER_COMPRESSED}/$fn-$i/$fntmptif" "${FOLDER_COMPRESSED}/$fn-$i/coeffs.csv"
 
 		for j in $@
 		do
@@ -32,13 +32,13 @@ do
 			then
 				fnfinaljxr="$fn-$i-$j.jxr"
 				
-				mkdir "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i-$j" && \
-				${PATH_JPEGXR} -o "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i-$j/$fntmptif" "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i/$fntmpjxr" && \
-				${PATH_JPEGXR} -c -o "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i-$j/$fnfinaljxr" -q $j "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i-$j/$fntmptif"
+				mkdir "${FOLDER_COMPRESSED}/$fn-$i-$j" && \
+				${PATH_JPEGXR} -o "${FOLDER_COMPRESSED}/$fn-$i-$j/$fntmptif" "${FOLDER_COMPRESSED}/$fn-$i/$fntmpjxr" && \
+				${PATH_JPEGXR} -c -o "${FOLDER_COMPRESSED}/$fn-$i-$j/$fnfinaljxr" -q $j "${FOLDER_COMPRESSED}/$fn-$i-$j/$fntmptif"
 
 				#touch $fntmpjxr $fntmptif $fnfinaljxr
 				echo "Double-compressed file $f using quantisation combination ($i, $j). Result file: $fnfinaljxr" && \
-				rm -f "${FOLDER_DOUBLE_COMPRESSED}/$fn-$i-$j/$fntmptif" "${FOLDER_DOUBLE_COMPRESSED}$fn-$i-$j/coeffs.csv"
+				rm -f "${FOLDER_COMPRESSED}/$fn-$i-$j/$fntmptif" "${FOLDER_COMPRESSED}/$fn-$i-$j/coeffs.csv"
 			fi
 		done
 	done
