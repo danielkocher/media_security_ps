690e677903219be86e2580d92310d3fd0ecf3ac8
Daniel Kocher <Daniel.Kocher@stud.sbg.ac.at>
2015-04-30 16:58:30 +0200

    Removed clean.sh and doing cleanup in Makefile now

--- double_compression_detection/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# echo on
-set -x
-
-find test_images -name "*.tif" -o -name "*.csv" -o -name "*.dat" -o -name "*.png" -o -name "*.gnu" -o -name "*.sh" -o -name "quantify" -type f | xargs rm -f
